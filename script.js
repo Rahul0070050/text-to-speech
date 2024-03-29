94899dd8d38369491af9e2e9eeda1a91664861b1
rahul <rahulor2000@gmail.com>
2024-03-29 22:36:36 +0530

    updated

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ window.speechSynthesis.onvoiceschanged = () => {
   typeOfVoices.forEach((voice, idx) => {
     voiceSelector.options[idx] = new Option(voice.name, idx);
   });
+  alert(typeOfVoices[0]);
   speeech.voice = typeOfVoices[0];
 };
 
